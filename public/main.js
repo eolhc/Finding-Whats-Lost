aad72315fd83528f6725cb78dcdddd72da8e5422
Chloe Phua <cqphua@gmail.com>
2017-01-16 15:19:12 +1100

    clean main js

--- public/main.js
+++ public/main.js
@@ -1,31 +1,7 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2016, Codrops
- * http://www.codrops.com
- */
+
 ;(function(window) {
 
 	'use strict';
-
-	// helper functions
-	// from https://davidwalsh.name/vendor-prefix
-	var prefix = (function () {
-		var styles = window.getComputedStyle(document.documentElement, ''),
-			pre = (Array.prototype.slice.call(styles).join('').match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']))[1],
-			dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
-
-		return {
-			dom: dom,
-			lowercase: pre,
-			css: '-' + pre + '-',
-			js: pre[0].toUpperCase() + pre.substr(1)
-		};
-	})();
 
 	// vars & stuff
 	var support = {transitions : Modernizr.csstransitions},
@@ -48,58 +24,14 @@
 		},
 		// the office element
 		office = document.querySelector('.office'),
-		// office´s levels wrapper
 		officeLevelsEl = office.querySelector('.levels'),
-		// office´s levels
 		officeLevels = [].slice.call(officeLevelsEl.querySelectorAll('.level')),
-		// total levels
 		officeLevelsTotal = officeLevels.length,
-		// // surroundings elems
-		// officeSurroundings = [].slice.call(office.querySelectorAll('.surroundings')),
-		// selected level position
 		selectedLevel,
-		// navigation element wrapper
-		// officeNav = document.querySelector('.officenav'),
-		// // show all office´s levels ctrl
-		// allLevelsCtrl = officeNav.querySelector('.officenav__button--all-levels'),
-		// // levels navigation up/down ctrls
-		// levelUpCtrl = officeNav.querySelector('.officenav__button--up'),
-		// levelDownCtrl = officeNav.querySelector('.officenav__button--down'),
-		// rooms
 		rooms = [].slice.call(officeLevelsEl.querySelectorAll('.room')),
-		// content element
-		// contentEl = document.querySelector('.content'),
-		// // content close ctrl
-		// contentCloseCtrl = contentEl.querySelector('button.content__button'),
-		// // check if a content item is opened
-		// isOpenContentArea,
-		// // check if currently animating/navigating
-		// isNavigating,
-		// // check if all levels are shown or if one level is shown (expanded)
 		isExpanded,
-		// // spaces list element
-		// spacesListEl = document.getElementById('spaces-list'),
-		// // spaces list ul
-		// spacesEl = spacesListEl.querySelector('ul.list'),
-		// // all the spaces listed
-		// spaces = [].slice.call(spacesEl.querySelectorAll('.list__item > a.list__link')),
-		// // reference to the current shows space (name set in the data-name attr of both the listed spaces and the rooms on the map)
-		// spaceref,
-		// // sort by ctrls
-		// sortByNameCtrl = document.querySelector('#sort-by-name'),
-		// // listjs initiliazation (all office´s spaces)
-		// spacesList = new List('spaces-list', { valueNames: ['list__link', { data: ['level'] }, { data: ['category'] } ]} ),
-
-		// sofficeer screens:
-		// open search ctrl
-		// openSearchCtrl = document.querySelector('button.open-search'),
-		// main container
 		containerEl = document.querySelector('.container');
-		// close search ctrl
-		// closeSearchCtrl = spacesListEl.querySelector('button.close-search');
-
 	function init() {
-		// init/bind events
 		initEvents();
 	}
 
@@ -171,30 +103,6 @@
 		// hide level rooms
 		removeRooms();
 
-		// shows surrounding element
-		// showSurroundings();
-
-		// hide office nav ctrls
-		// hideMallNav();
-
-		// show back the complete list of spaces
-		// spacesList.filter();
-
-		// close content area if it is open
-		// if( isOpenContentArea ) {
-		// 	closeContentArea();
-		// }
-	}
-
-	/**
-	 * Shows all spaces for current level
-	 */
-	// function showLevelSpaces() {
-	// 	spacesList.filter(function(item) {
-	// 		return item.values().level === selectedLevel.toString();
-	// 	});
-	// }
-
 	/**
 	 * Shows the level´s rooms
 	 */
@@ -212,41 +120,6 @@
 		classie.remove(levelEl.querySelector('.level__rooms'), 'level__rooms--active');
 	}
 
-	/**
-	 * Show the navigation ctrls
-	 */
-	// function showMallNav() {
-	// 	classie.remove(officeNav, 'officenav--hidden');
-	// }
-
-	/**
-	 * Hide the navigation ctrls
-	 */
-	// function hideMallNav() {
-	// 	classie.add(officeNav, 'officenav--hidden');
-	// }
-
-	/**
-	 * Show the surroundings level
-	 */
-	// function showSurroundings() {
-	// 	officeSurroundings.forEach(function(el) {
-	// 		classie.remove(el, 'surroundings--hidden');
-	// 	});
-	// }
-
-	/**
-	 * Hide the surroundings level
-	 */
-	// function hideSurroundings() {
-	// 	officeSurroundings.forEach(function(el) {
-	// 		classie.add(el, 'surroundings--hidden');
-	// 	});
-	// }
-
-	/**
-	 * Navigate through the office´s levels
-	 */
 	function navigate(direction) {
 		if( isNavigating || !isExpanded || isOpenContentArea ) {
 			return false;
